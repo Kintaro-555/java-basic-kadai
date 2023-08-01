08df32ae1dab3d4b916bab8ce9cd9fd9461b9856
kodai-555 <koudai1925@icloud.com>
2023-08-01 23:34:25 +0900

    kadai_008を編集

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -7,7 +7,14 @@ public class Price_Chapter08 {
 		int serviceCost=3000;
 		
 		switch(userAge){
+			case 10 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 20 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
 			case 30 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 40 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 50 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 60 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 70 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
+			case 80 -> System.out.println(userAge+"代の料金は"+serviceCost+"円");
 		default -> System.out.println("500円");
 		}
 	}
