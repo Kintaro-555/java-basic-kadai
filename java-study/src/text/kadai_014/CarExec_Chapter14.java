0437535a8303fe9fa76cef48a1b30d31e6a01fb1
kodai-555 <koudai1925@icloud.com>
2023-08-06 01:43:26 +0900

    kadai_014を提出

---
+++ java-study/src/text/kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,12 @@
+package text.kadai_014;
+
+public class CarExec_Chapter14 {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter14 car = new Car_Chapter14();
+		car.gearChange(5);
+		
+		car.run();
+		}
+	
+}
